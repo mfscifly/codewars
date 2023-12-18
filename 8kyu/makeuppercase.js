a8356dabd1e0c34dbd5ff48af28c421a4b17a94b
mfscifly <138173334+mfscifly@users.noreply.github.com>
2023-12-18 12:35:57 -0800

    added codewars

---
+++ 8kyu/makeuppercase.js
@@ -0,0 +1,10 @@
+//Write a function which converts the input string to uppercase.
+//my solution:
+
+const makeUpperCase = str => str.toUpperCase();
+
+//alternate:
+
+function makeUpperCase2 (str){
+    return str.toUpperCase();
+}
