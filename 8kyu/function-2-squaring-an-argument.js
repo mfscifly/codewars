9a68a0304e2eb4d3bf32a494a069b9f78ffbb6bf
mfscifly <138173334+mfscifly@users.noreply.github.com>
2024-01-24 13:28:13 -0500

    added daily challenge

---
+++ 8kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,6 @@
+// Link to challenge: https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// My solution:
+
+let square = x => x*x;
