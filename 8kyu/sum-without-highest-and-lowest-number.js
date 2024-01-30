89636d94bebf3fea3b8ba9eeeb158d689e3f09b1
mfscifly <138173334+mfscifly@users.noreply.github.com>
2024-01-30 13:28:13 -0500

    added daily challenge

---
+++ 8kyu/sum-without-highest-and-lowest-number.js
@@ -0,0 +1,35 @@
+// Task
+
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+// My refactored solution:
+
+function sumArray(array) {
+    if (!array || array.length <3){
+      return 0;
+    }
+    return array.sort((a,b) => a-b).slice(1,-1).reduce((acc,cV) => acc+cV, 0);
+}
+
+// My original solution:
+
+function sumArray(array) {
+    if (!array || array.length < 3){
+      return 0;
+    }
+    array.sort((a,b)=>a-b).pop();
+    array.shift();
+    return array.reduce((acc,cV) => acc+cV, 0);
+}
