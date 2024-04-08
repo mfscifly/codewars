ed870bd7c5feb93926354cc6d89e8da448758894
mfscifly <138173334+mfscifly@users.noreply.github.com>
2024-04-07 19:59:16 -0700

    added daily challenge

---
+++ 6kyu/find-the-unique-number.js
@@ -0,0 +1,27 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+// This is the first kata in series:
+
+// Find the unique number (this kata)
+// Find the unique string
+// Find The Unique
+
+
+// My solution:
+
+function findUniq(arr) {
+    return Number(arr.filter(num => arr.indexOf(num) === arr.lastIndexOf(num)));
+}
+
+// My alternate solution:
+
+function findUniq(arr) {
+    arr.sort((a,b)=>a-b);
+    return arr[0]===arr[1] ? arr.pop() : arr[0];
+}
