bdfb4be22c66f0411939ff2539c66127dac1b44c
mfscifly <138173334+mfscifly@users.noreply.github.com>
2024-04-13 17:48:45 -0700

    added daily challenge

---
+++ 6kyu/are-they-the-same.js
@@ -0,0 +1,59 @@
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+// Examples
+
+// Valid arrays
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11] 
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+// Invalid arrays
+
+// If, for example, we change the first number to something else, comp is not returning true anymore:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+// Remarks
+
+// a or b might be [] or {} (all languages except R, Shell).
+// a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+// If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+// My solution:
+
+function comp(a, b) {
+    if (!a || !b){
+      return false;
+    }
+    a = a.map(x=>x*x).sort((a,b)=>a-b).join();
+    b.sort((a,b)=>a-b).join();
+    return a==b;
+  }
+
+// My original solution:
+
+function comp(array1, array2){  
+    //check for null values
+    if (!array1 || !array2) {
+        return false;
+    }
+    //change array2 to sqrt values so both arrays are equal
+    array2 = array2.map(num => Math.sqrt(num));
+    //loop array1 and check if value appears in array2, if so remove from array2
+    for (let i=0; i<array1.length; i++){
+     if (array2.indexOf(array1[i]) >= 0) {
+       array2.splice(array2.indexOf(array1[i]),1);
+     }
+    }
+    //check if anything is left in array2 
+    return !array2.length;
+}
