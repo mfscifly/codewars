68b9237249a8d01e70dda84baa608059121afdb5
mfscifly <138173334+mfscifly@users.noreply.github.com>
2024-04-20 13:28:13 -0500

    added daily challenge

---
+++ 7kyu/find-the-capitals.js
@@ -0,0 +1,21 @@
+// Instructions
+
+// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+// Example (Input --> Output)
+
+// "CodEWaRs" --> [0,3,4,6]
+
+// My solution:
+
+let capitals = function (word) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+    let result = [];
+    word = word.split('');
+    for (let i=0; i<word.length; i++){
+      if (alphabet.includes(word[i])){
+        result.push(i);
+      }
+    }
+    return result;
+};
