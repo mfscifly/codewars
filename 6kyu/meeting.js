6dd44ed23f899728e1b4eb75f81d870ff8d0909f
mfscifly <138173334+mfscifly@users.noreply.github.com>
2024-06-17 20:11:05 -0700

    added daily challenge

---
+++ 6kyu/meeting.js
@@ -0,0 +1,23 @@
+// Link to challenge: https://www.codewars.com/kata/59df2f8f08c6cec835000012/train/javascript
+
+// Description:
+
+// John has invited some friends. His list is:
+
+// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+// Could you make a program that
+
+// makes this string uppercase
+// gives it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+// So the result of function meeting(s) will be:
+
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+// It can happen that in two distinct families with the same family name two people have the same first name too.
+
+// My solution:
+
+function meeting(s) {
+    return s.toUpperCase().split(';').map(name => name = '(' + name.split(':').reverse().join(', ') + ')').sort().join('');
+}
